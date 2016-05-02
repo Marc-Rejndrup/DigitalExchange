12eb00a194adc790822904e9605b1ace05264196
MarcRejndrup <marc.rejndrup@stonybrook.edu>
2016-05-02 16:12:34 -0400

    track stuff properly?

---
+++ CSE305_update/src/dataType/DataTypeHolding.java
@@ -0,0 +1,26 @@
+package dataType;
+
+public class DataTypeHolding {
+	private String stockSymbol;
+	private String amount;
+	private String accountId;
+	
+	public String getStockSymbol() {
+		return stockSymbol;
+	}
+	public void setStockSymbol(String stockSymbol) {
+		this.stockSymbol = stockSymbol;
+	}
+	public String getAmount() {
+		return amount;
+	}
+	public void setAmount(String amount) {
+		this.amount = amount;
+	}
+	public String getAccountId() {
+		return accountId;
+	}
+	public void setAccountId(String accountId) {
+		this.accountId = accountId;
+	}
+}
