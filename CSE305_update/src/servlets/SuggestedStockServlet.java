4c54ed1d91f192841b0491083eb1cd4bbe6e0204
MarcRejndrup <marc.rejndrup@stonybrook.edu>
2016-05-02 22:08:23 -0400

    All the useless servlets and some reasonable jsps.

--- CSE305_update/src/servlets/SuggestedStockServlet.java
+++ CSE305_update/src/servlets/SuggestedStockServlet.java
@@ -1,4 +1,5 @@
 package servlets;
+
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
@@ -9,44 +10,43 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import dataType.DataTypeHolding;
-public class ClientHoldingServlet extends HttpServlet{
-	 protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+public class SuggestedStockServlet extends HttpServlet {//might need to handle doGet.
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	 	HttpSession session=request.getSession();
-	 	//always
 		String loginID = ""+session.getAttribute("login");
 		String accountID = ""+session.getAttribute("account");
 		String mysJDBCDriver = "com.mysql.jdbc.Driver"; 
 		String mysURL ="jdbc:mysql://127.0.0.1:3306/cse305";
 		String mysUserID = "root"; 
 		String mysPassword = "1234";
-		//end always
 		//get Parameters
-		//String ssn = request.getParameter("empssn");
+		String id = request.getParameter("clientID");
+		if(id == null)
+			id = accountID;
 		java.sql.Connection conn = null;
 		try {
 			Class.forName(mysJDBCDriver).newInstance();
 			java.util.Properties sysprops=System.getProperties();
 			sysprops.put("user",mysUserID);
 			sysprops.put("password",mysPassword);
-			conn = java.sql.DriverManager.getConnection(mysURL,sysprops);
+			conn=java.sql.DriverManager.getConnection(mysURL,sysprops);
 			System.out.println("Connected successfully to database using JConnect");
-
+			java.sql.ResultSet rs;
 			java.sql.Statement stmt1=conn.createStatement();
-			java.sql.ResultSet rs = stmt1.executeQuery("SELECT * FROM HOLDING WHERE AccountId = "+accountID);
-			List<DataTypeHolding> list = new ArrayList<DataTypeHolding>();
+			//make query
+			rs = stmt1.executeQuery("SELECT * FROM HOLDING WHERE AccountId = "+accountID);//change this!
+			//type the list.
+			//List<x> list = new ArrayList<x>();
 			while(rs.next()){
-				DataTypeHolding data = new DataTypeHolding();
-				data.setStockSymbol(rs.getString(1));
-				data.setAmount(rs.getString(2));
-				data.setAccountId(rs.getString(3));
-				list.add(data);
+				//build list
 			}
-			request.setAttribute("HoldingTable", list);
+			//request.setAttribute("TableNAME", list);
 			rs.close();
 			conn.close();
-		} catch(Exception e){
+		}catch(Exception e){
 			e.printStackTrace();
-		} finally{
+		}finally{
 			try{conn.close();}catch(Exception ee){};
 		}
 		RequestDispatcher view = request.getRequestDispatcher("clientHolding.jsp");
